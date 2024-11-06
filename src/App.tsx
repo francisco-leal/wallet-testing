047df4c54805b436f48506ed9b44d2b4df7f258a
Francisco Leal <francisco@talentprotocol.com>
2024-11-06 11:48:27 +0000

    Add message signing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
-import { useAccount, useConnect, useDisconnect, useSwitchChain } from "wagmi";
+import {
+  useAccount,
+  useConnect,
+  useDisconnect,
+  useSignMessage,
+  useSwitchChain,
+} from "wagmi";
 import { base, baseSepolia } from "viem/chains";
+import { useState } from "react";
+import { verifyMessage } from "@wagmi/core";
+import { config } from "./wagmi";
 
 function App() {
   const account = useAccount();
   const { connectors, connect, status, error } = useConnect();
   const { disconnect } = useDisconnect();
   const { switchChain } = useSwitchChain();
+  const { signMessageAsync } = useSignMessage();
+  const [signature, setSignature] = useState<`0x${string}` | null>(null);
+  const [result, setResult] = useState<boolean | null>(null);
+
+  const handleSignMessage = async () => {
+    const signature = await signMessageAsync({
+      message: `Signing message from: ${account.address} on chain ${account.chain?.name}`,
+    });
+    setSignature(signature);
+    setResult(null);
+  };
+
+  const validateSignature = async () => {
+    if (!account.address) return;
+    if (!signature) return;
+
+    const result = await verifyMessage(config, {
+      address: account.address,
+      message: `Signing message from: ${account.address} on chain ${account.chain?.name}`,
+      signature,
+    });
+
+    console.log(result);
+    setResult(result);
+  };
 
   return (
     <>
@@ -50,6 +84,19 @@ function App() {
         <button onClick={() => switchChain({ chainId: base.id })}>
           Switch to Base
         </button>
+      </div>
+
+      <div>
+        <h2>Sign Message</h2>
+        <button onClick={handleSignMessage} disabled={!account.address}>
+          Sign Message
+        </button>
+        <button onClick={validateSignature} disabled={!signature}>
+          Validate Signature
+        </button>
+        {result !== null && (
+          <div>{result ? "Signature is valid" : "Signature is invalid"}</div>
+        )}
       </div>
     </>
   );
